d8916bad23ece3adb49240083985bb3d20e1eda9
christo <xtoswanepoel@gmail.com>
2023-06-13 17:57:10 +0200

    Frontend ready for testing

--- frontend/src/Chatroom.tsx
+++ frontend/src/Chatroom.tsx
@@ -1,17 +1,41 @@
 import styles from "./Chatroom.module.css";
-import {createSignal} from "solid-js";
+import {createSignal, For} from "solid-js";
 
 export const Chatroom = (props: { username: (string & {}); }) => {
     const [message, setMessage] = createSignal("");
     const [messages, setMessages] = createSignal([{ sender: "server", text: "hi!" }]);
 
-    //setMessages(msgs => [...msgs, { sender: "server", text: "hi!" }]);
+    // Websocket nonsense
+    let serverUrl = "ws://" + window.location.host + ":8081";
+    console.log(serverUrl)
+
+    const ws = new WebSocket(serverUrl);
+
+    ws.onopen = (event) => {
+        console.log("WS OPEN SUCCESSFUL");
+    }
+
+    ws.onerror = (event) => {
+        console.error("WS ERROR: " + event);
+    }
+
+    ws.onmessage = (event) => {
+        let msg = JSON.parse(event.data);
+        setMessages([...messages(), msg]);
+        console.log("RECEIVED MESSAGE");
+    }
+
     const handleSendMessage = (e: Event) => {
         e.preventDefault();
 
         if (message() != "") {
             console.log("sending message: " + message().trim());
             setMessage("");
+            let msg = {
+                sender: props.username,
+                text: message().trim(),
+            }
+            ws.send(JSON.stringify(msg));
         }
     }
 
@@ -19,7 +43,11 @@ export const Chatroom = (props: { username: (string & {}); }) => {
         <div class={styles.container}>
             <h1 class={styles.heading}>Chatroom</h1>
             <div class={styles.messagesView}>
-                messages
+                <For each={messages()}>
+                    {(msg) => <div class={styles.message}>
+                        <p><b>{msg.sender}:</b> {msg.text}</p>
+                    </div>}
+                </For>
             </div>
             <div class={styles.input}>
                 <form onSubmit={handleSendMessage}>
